50a62f6108f69f935c75e621ac2ded5fb7774d5a
Jason Choi <jasonnoahchoi@gmail.com>
2015-02-20 15:14:10 -0700

    Add DetailViewController and set up xib with label, tableview, and textfield

---
+++ Time-Tracker/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Time-Tracker
+//
+//  Created by Jason Noah Choi on 2/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ Time-Tracker/DetailViewController.m
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.m
+//  Time-Tracker
+//
+//  Created by Jason Noah Choi on 2/20/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
